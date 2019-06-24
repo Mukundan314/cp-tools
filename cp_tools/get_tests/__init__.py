f70bf793b35d86f8a8cb7fa01a1005f9f6f8ae07
Mukundan Senthil <mukundan314@gmail.com>
2019-06-24 07:52:18 +0530

    Add testcase parser for codeforces

--- cp_tools/get_tests/__init__.py
+++ cp_tools/get_tests/__init__.py
@@ -1,8 +1,10 @@
-from . import atcoder
+from . import atcoder, codeforces
 
 
 def get_tests(provider, **kwargs):
     if provider == "atcoder":
         return atcoder.get_tests(kwargs["contest"], kwargs["task"])
+    if provider == "codeforces":
+        return codeforces.get_tests(kwargs["contest"], kwargs["index"])
 
     raise ValueError("Unknown provider {}".format(provider))
---
+++ cp_tools/get_tests/codeforces.py
@@ -0,0 +1,18 @@
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+
+def get_tests(contest, problem):
+    url = "https://codeforces.com/contest/{}/problem/{}".format(
+        contest, problem)
+
+    with urllib.request.urlopen(url) as res:
+        soup = BeautifulSoup(res.read(), "html.parser")
+
+        inputs = map(lambda x: x.pre.get_text().lstrip(),
+                     soup.find_all(class_="input"))
+        outputs = map(lambda x: x.pre.get_text().lstrip(),
+                      soup.find_all(class_="output"))
+
+    return zip(inputs, outputs)

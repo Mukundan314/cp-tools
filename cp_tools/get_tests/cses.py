a9c56f374f8cb3ce409e5f4cc742f49f37665c5b
Mukundan Senthil <mukundan314@gmail.com>
2019-09-13 14:54:08 +0530

    Add support for cses contests

--- cp_tools/get_tests/cses.py
+++ cp_tools/get_tests/cses.py
@@ -3,8 +3,11 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
-def get_tests(task):
-    url = "https://cses.fi/problemset/task/{}".format(task)
+def get_tests(task, contest=None):
+    if contest is None:
+        url = "https://cses.fi/problemset/task/{}".format(task)
+    else:
+        url = "https://cses.fi/{}/task/{}".format(contest, task)
 
     with urllib.request.urlopen(url) as res:
         soup = BeautifulSoup(res.read(), "html.parser")
